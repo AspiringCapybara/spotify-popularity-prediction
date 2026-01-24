1be817cc547ebc4aaa1b5d00ac4a3234b9e1b2a1
AspiringCapybara <jj1876033@gmail.com>
2026-01-24 15:31:59 +0000

    Cleaned up comments in HTML, CSS and JavaScript code for portfolio

--- project/data_processing/data_pipeline.py
+++ project/data_processing/data_pipeline.py
@@ -3,11 +3,28 @@ import seaborn as sns
 import pandas as pd
 import numpy as np
 import statsmodels.api as sm
+import os
+
 from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
 from pandasql import sqldf
 from statsmodels.stats.outliers_influence import variance_inflation_factor
-from data_processing.cleaning import df_no_nulls
 
+
+current_dir = os.path.dirname(__file__)
+csv_path = os.path.join(current_dir, 'popular_spotify_songs.csv')
+
+# Loading the dataset
+try:
+    df = pd.read_csv(csv_path, encoding='utf-8')
+except UnicodeDecodeError:
+    try:
+        df = pd.read_csv(csv_path, encoding='latin1')
+    except UnicodeDecodeError:
+        df = pd.read_csv(csv_path, encoding='cp1252')
+
+# Cleaning the data
+df.drop_duplicates('track_name')
+df_no_nulls = df.dropna(how='any')
 
 # Feature selection for model
 df_cleaned = df_no_nulls.drop(['track_name', 'artist(s)_name', 'artist_count', 'released_day', 'in_spotify_charts',
