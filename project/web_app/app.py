ad58b1980366723d76f8a7f814b20e591231c763
AspiringCapybara <jj1876033@gmail.com>
2026-02-03 09:39:00 +0000

    Fixed problem of container being killed by Flask reloader

--- project/web_app/app.py
+++ project/web_app/app.py
@@ -109,5 +109,5 @@ def index():
         return render_template('index.html', streams=predicted_streams)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=False)
