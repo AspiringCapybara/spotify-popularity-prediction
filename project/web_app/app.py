c6f9d987f381e5f36236ccd2aa5f5e307cb0ed63
AspiringCapybara <jj1876033@gmail.com>
2026-02-19 14:08:20 +0000

    Removed code that constructs DataFrame to be inputted into model. Input DataFrame is now handled by preprocess_input()

--- project/web_app/app.py
+++ project/web_app/app.py
@@ -34,66 +34,19 @@ def index():
             ]
         )
 
-        server_side_validation(num_playlists, release_year, release_month, mode, key,
-                               tempo, energy, danceability, valence, acousticness, liveness, speechiness)
-
-        (
-            release_month_2, release_month_3, release_month_4, release_month_5,
-            release_month_6, release_month_7, release_month_8, release_month_9,
-            release_month_10, release_month_11, release_month_12,
-            mode,
-            key_Asharp, key_B, key_Csharp, key_D, key_Dsharp, key_E,
-            key_F, key_Fsharp, key_G, key_Gsharp,
-            tempo, energy, danceability, valence,
-            acousticness, liveness, speechiness
-        ) = preprocess_input(
-            release_month, mode, key,
-            tempo, energy, danceability, valence,
-            acousticness, liveness, speechiness
+        server_side_validation(
+            num_playlists, release_year, release_month,
+            mode, key, tempo, energy, danceability,
+            valence, acousticness, liveness, speechiness
         )
 
-        preprocessed_colnames = X.columns.tolist()
-        input_data = pd.DataFrame(columns=preprocessed_colnames)
-        for_prediction = [
-            ('in_spotify_playlists', num_playlists),
-            ('released_year', release_year),
-            ('release_month_2', release_month_2),
-            ('release_month_3', release_month_3),
-            ('release_month_4', release_month_4),
-            ('release_month_5', release_month_5),
-            ('release_month_6', release_month_6),
-            ('release_month_7', release_month_7),
-            ('release_month_8', release_month_8),
-            ('release_month_9', release_month_9),
-            ('release_month_10', release_month_10),
-            ('release_month_11', release_month_11),
-            ('release_month_12', release_month_12),
-            ('mode', mode),
-            ('key_A#', key_Asharp),
-            ('key_B', key_B),
-            ('key_C#', key_Csharp),
-            ('key_D', key_D),
-            ('key_D#', key_Dsharp),
-            ('key_E', key_E),
-            ('key_F', key_F),
-            ('key_F#', key_Fsharp),
-            ('key_G', key_G),
-            ('key_G#', key_Gsharp),
-            ('bpm', tempo),
-            ('energy_%', energy),
-            ('danceability_%', danceability),
-            ('valence_%', valence),
-            ('acousticness_%_log', acousticness),
-            ('liveness_%_log', liveness),
-            ('speechiness_%_log', speechiness)
-        ]
+        input_df = preprocess_input(
+            num_playlists, release_year, release_month,
+            mode, key, tempo, energy, danceability,
+            valence, acousticness, liveness, speechiness
+        )
 
-        input_values_dict = {}
-        for colname, value in for_prediction:
-            input_values_dict[colname] = value
-        input_data = pd.DataFrame([input_values_dict])
-        input_data = input_data.reindex(columns=X.columns, fill_value=0)
-        predicted_streams_arr = model.predict(input_data)
+        predicted_streams_arr = model.predict(input_df)
         predicted_streams = round(np.exp(predicted_streams_arr[0]))
 
         return render_template('index.html', streams=predicted_streams)
