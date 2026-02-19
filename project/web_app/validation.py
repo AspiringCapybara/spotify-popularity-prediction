8e27995d151ae2405f9967d297b740475798bda9
AspiringCapybara <jj1876033@gmail.com>
2026-02-19 13:31:56 +0000

    Replaced print statements with raise ValueError

--- project/web_app/validation.py
+++ project/web_app/validation.py
@@ -27,14 +27,14 @@ def server_side_validation(num_playlists, release_year, release_month, mode, key
     try:
         num_playlists = int(num_playlists)
     except ValueError:
-        print("Number of playlists must be an integer.")
+        raise ValueError("Number of playlists must be an integer.")
     if num_playlists < 31 or num_playlists > 52898:
         raise ValueError("Number of playlists entered is out of range.")
 
     try:
         release_year = int(release_year)
     except ValueError:
-        print("Release year must be an integer.")
+        raise ValueError("Release year must be an integer.")
     current_year = dt.datetime.now().year
     # Songs in training data are released from 1930 onwards
     if release_year > current_year or release_year < 1930:
@@ -59,48 +59,48 @@ def server_side_validation(num_playlists, release_year, release_month, mode, key
     try:
         tempo = int(tempo)
     except ValueError:
-        print("Song tempo (in bpm) must be an integer.")
+        raise ValueError("Song tempo (in bpm) must be an integer.")
     if tempo < 65 or tempo > 206:
         raise ValueError("Tempo entered is out of range.")
 
     try:
         energy = int(energy)
     except ValueError:
-        print("Energy (in %) must be an integer.")
+        raise ValueError("Energy (in %) must be an integer.")
     if energy < 14 or energy > 97:
         raise ValueError("Energy entered is out of range.")
 
     try:
         danceability = int(danceability)
     except ValueError:
-        print("Danceability (in %) must be an integer.")
+        raise ValueError("Danceability (in %) must be an integer.")
     if danceability < 23 or danceability > 96:
         raise ValueError("Danceability entered is out of range.")
 
     try:
         valence = int(valence)
     except ValueError:
-        print("Valence (in %) must be an integer.")
+        raise ValueError("Valence (in %) must be an integer.")
     if valence < 4 or valence > 97:
         raise ValueError("Valence entered is out of range.")
 
     try:
         acousticness = int(acousticness)
     except ValueError:
-        print("Acousticness (in %) must be an integer.")
+        raise ValueError("Acousticness (in %) must be an integer.")
     if acousticness < 0 or acousticness > 97:
         raise ValueError("Acousticness entered is out of range.")
 
     try:
         liveness = int(liveness)
     except ValueError:
-        print("Liveness (in %) must be an integer.")
+        raise ValueError("Liveness (in %) must be an integer.")
     if liveness < 3 or liveness > 97:
         raise ValueError("Liveness entered is out of range.")
 
     try:
         speechiness = int(speechiness)
     except ValueError:
-        print("Speechiness (in %) must be an integer.")
+        raise ValueError("Speechiness (in %) must be an integer.")
     if speechiness < 2 or speechiness > 64:
         raise ValueError("Speechiness entered is out of range.")
